9308971b339f07f3d0f854a8c2510b39ff60e6f8
Hassan-Ayman-SE <hsankraz66@gmail.com>
2024-08-08 10:16:07 +0000

    Config base server And ADD jsmine config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World Hassan World");
+import express, { Application, Request, Response } from 'express'
+
+const PORT = 3000
+// create an instance server
+const app: Application = express()
+
+// add routing for / path
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World 🌍',
+  })
+})
+
+// start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot:${PORT}`)
+})
+export default app
