ad682c5fcd032c2043356e09905e1d7ae31a5276
Hassan-Ayman-SE <hsankraz66@gmail.com>
2024-08-08 20:51:39 +0000

    handling errors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import rateLimit from 'express-rate-limit'
+
+import errorMiddleware from './middleware/error.middleware'
 const PORT = 3000
 // create an instance server
 const app: Application = express()
@@ -38,6 +40,15 @@ app.post('/', (req: Request, res: Response) => {
   })
 })
 
+// error handler middleware
+app.use(errorMiddleware)
+
+app.use((_: Request, res: Response) => {
+  res.status(404).json({
+    message:
+      'Ohh you are lost, read the API documentation to find your way back home 😂',
+  })
+})
 // start express server
 app.listen(PORT, () => {
   console.log(`Server is starting at prot:${PORT}`)
